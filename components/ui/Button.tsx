ea31edc9ee2bd6fff3d382543c94fedd8889e9f8
Ankit Singh <138374889+thetribalgod@users.noreply.github.com>
2025-02-23 03:58:05 +0530

    Update Button.tsx

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from "react";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  // Add any additional props specific to your Button component here
-}
-
-const Button: React.FC<ButtonProps> = ({ children, className, ...props }) => {
+const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ 
+  children, className, ...props 
+}) => {
   return (
     <button className={className} {...props}>
       {children}
